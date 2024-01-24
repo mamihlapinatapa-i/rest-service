9aa418360dab4eb70db9cd1159d14fc19026409f
NMW <mwnam@hecto.co.kr>
2024-01-24 14:17:42 +0900

    View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
